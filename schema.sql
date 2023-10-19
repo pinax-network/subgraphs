dfc588bca23d4bd600242e8357ea3c1ca2bb7cef
Denis <carriere.denis@gmail.com>
2023-10-18 22:11:14 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,6 @@
 CREATE TABLE IF NOT EXISTS Clock (
-    id FixedString(40),
-    number String,
-    seconds String,
-    nanos String,
-    timestamp String
+    seconds UInt32,
+    nanos UInt16,
 )
-ENGINE = ReplacingMergeTree()
-ORDER BY (id)
+ENGINE = MergeTree()
+ORDER BY (seconds)
